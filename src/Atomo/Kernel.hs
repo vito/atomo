1b59cbb3c3e5d24be35ebe740fb0b5183b902633
Alex Suraci <i.am@toogeneric.com>
2010-09-05 16:05:16 +0000

    Added Expression@as:Pattern, Pattern@matches?:, and @match:.

--- src/Atomo/Kernel.hs
+++ src/Atomo/Kernel.hs
@@ -124,6 +124,7 @@ load = do
     loadMessage
     loadComparable
     loadParticle
+    loadPattern
     loadPorts
   where
     joinWith t (Block s ps es) as
@@ -881,6 +882,57 @@ loadList = do
         liftIO . writeIORef l $ V.snoc vs v
 
         return (List l)
+
+loadPattern :: VM ()
+loadPattern = do
+    [$p|(e: Expression) as: Pattern|] =: do
+        Expression e <- here "e" >>= findValue isExpression
+        p <- toPattern e
+        return (Pattern p)
+
+    [$p|(p: Pattern) matches?: v|] =: do
+        Pattern p <- here "p" >>= findValue isPattern
+        v <- here "v"
+        ids <- gets primitives
+
+        if match ids p v
+            then do
+                obj <- eval [$e|Object clone|]
+                o <- newObject $ \o -> o
+                    { oDelegates = [obj]
+                    , oMethods = (toMethods (bindings' p v), M.empty)
+                    }
+
+                return (keyParticle ["yes"] [Nothing, Just o])
+            else return (particle "no")
+  where
+    -- convert an expression to the pattern match it represents
+    toPattern (Dispatch { eMessage = EKeyword { emNames = ["."], emTargets = [h, t] } }) = do
+        hp <- toPattern h
+        tp <- toPattern t
+        return (PHeadTail hp tp)
+    toPattern (Dispatch { eMessage = EKeyword { emNames = [n], emTargets = [ETop {}, x] } }) = do
+        p <- toPattern x
+        return (PNamed n p)
+    toPattern (Dispatch { eMessage = ESingle { emName = "_" } }) =
+        return PAny
+    toPattern (Dispatch { eMessage = ESingle { emName = n } }) =
+        return (PNamed n PAny)
+    toPattern (EList { eContents = es }) = do
+        ps <- mapM toPattern es
+        return (PList ps)
+    toPattern (EParticle { eParticle = EPMSingle n }) =
+        return (PPMSingle n)
+    toPattern (EParticle { eParticle = EPMKeyword ns mes }) = do
+        ps <- forM mes $ \me ->
+            case me of
+                Nothing -> return PAny
+                Just e -> toPattern e
+
+        return (PPMKeyword ns ps)
+    toPattern (Primitive { eValue = v }) =
+        return (PMatch v)
+    toPattern e = throwError . ErrorMsg $ "don't know how to convert to pattern: " ++ show (e)
 
 loadPorts :: VM ()
 loadPorts = do
