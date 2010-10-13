4248a508409caee1716c9960f60b77f5ede16f43
Alex Suraci <i.am@toogeneric.com>
2010-10-13 23:36:50 +0000

    removed Makefile; cabal-install handles it now via postInst hook

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,29 @@
-import Distribution.Simple (defaultMain)
+import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
 
-main = defaultMain
+import System.Directory
+import System.Exit
+import System.FilePath
+
+main = defaultMainWithHooks simpleUserHooks
+    { postInst = installEco
+    }
+
+installEco :: Args -> InstallFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+installEco _ _ _ _ = do
+    home <- getHomeDirectory
+
+    mapM_ (createDirectoryIfMissing True . (ecosystem home </>))
+        ["bin", "lib"]
+
+    copyFile eco (toEco home)
+
+    from <- getPermissions (toEco home)
+    setPermissions (toEco home) (from { executable = True })
+  where
+    ecosystem h = h </> ".eco"
+
+    eco = "bin" </> "eco"
+    toEco h = ecosystem h </> eco
