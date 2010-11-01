ea9894b1e20a89c861945b09ab32ba3f551520c5
Alex Suraci <i.am@toogeneric.com>
2010-11-01 14:04:30 +0000

    moved exception kernel to prelude

--- src/Atomo/Kernel/Exception.hs
+++ src/Atomo/Kernel/Exception.hs
@@ -7,33 +7,3 @@ import Atomo
 load :: VM ()
 load = do
     [$p|raise: v|] =: here "v" >>= throwError . Error
-
-    {-[$p|(action: Block) catch: (recover: Block)|] =:-}
-        {-catchError (eval [$e|action call|]) $ \err -> do-}
-            {-modify $ \s -> s { stack = [] }-}
-
-            {-recover <- here "recover"-}
-            {-dispatch (keyword ["call"] [recover, list [asValue err]])-}
-
-    {-[$p|(action: Block) catch: (recover: Block) ensuring: (cleanup: Block)|] =:-}
-        {-catchError-}
-            {-(do r <- eval [$e|action call|]-}
-                {-eval [$e|cleanup call|]-}
-                {-return r) $ \err -> do-}
-            {-modify $ \s -> s { stack = [] }-}
-
-            {-recover <- here "recover"-}
-
-            {-res <- dispatch (keyword ["call"] [recover, list [asValue err]])-}
-            {-eval [$e|cleanup call|]-}
-            {-return res-}
-
-    {-[$p|(action: Block) ensuring: (cleanup: Block)|] =:-}
-        {-catchError-}
-            {-(do r <- eval [$e|action call|]-}
-                {-eval [$e|cleanup call|]-}
-                {-return r)-}
-            {-(\err -> eval [$e|cleanup call|] >> throwError err)-}
-
-
-
