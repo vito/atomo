e8476dcf886552b2e10ff0304e760bdf44ec612f
Mathnerd314 <mathnerd314.gph+github@gmail.com>
2010-11-25 06:15:57 +0000

    Replace VMT with StateT

--- src/Atomo/VMT.hs
+++ src/Atomo/VMT.hs
@@ -8,49 +8,24 @@ import Atomo.Types
 
 -- | A monad transformer for Atomo's VM which just passes around the VM
 -- environment.
-newtype VMT m a =
-    VMT
-        { runVMT :: Env -> m (a, Env)
-        }
-
-instance Monad m => Monad (VMT m) where
-    x >>= f = VMT $ \e -> do
-        (v, e') <- runVMT x e
-        runVMT (f v) e'
-
-    return x = VMT $ \e -> return (x, e)
-
-instance MonadTrans VMT where
-    lift m = VMT $ \e -> do
-        a <- m
-        return (a, e)
-
-instance MonadIO m => MonadIO (VMT m) where
-    liftIO f = VMT $ \e -> do
-        x <- liftIO f
-        return (x, e)
-
-instance MonadCont m => MonadCont (VMT m) where
-    callCC f = VMT $ \e ->
-        callCC $ \c -> runVMT (f (\a -> VMT (\e' -> c (a, e')))) e
-
+type VMT = StateT Env
 
 -- | Execute a VM action inside a VMT monad.
 vm :: MonadIO m => VM Value -> VMT m Value
-vm x = VMT (liftIO . runStateT (runContT x return))
+vm x = StateT (liftIO . runStateT (runContT x return))
 
 -- | Like `vm', but ignores the result.
 vm_ :: MonadIO m => VM a -> VMT m ()
 vm_ x = vm (x >> return (particle "ok")) >> return ()
 
--- | Grab the underlying VM envionment.
+-- | Grab the underlying VM environment.
 getEnv :: Monad m => VMT m Env
-getEnv = VMT $ \e -> return (e, e)
+getEnv = StateT $ \e -> return (e, e)
 
 -- | Replace the underlying VM environment.
 putEnv :: Monad m => Env -> VMT m ()
-putEnv e = VMT $ \_ -> return ((), e)
+putEnv e = StateT $ \_ -> return ((), e)
 
 -- | Execute a VMT monad with an initial environment, returning its result.
 execVM :: Monad m => VMT m a -> Env -> m a
-execVM x e = liftM fst (runVMT x e)
+execVM x e = liftM fst (runStateT x e)
