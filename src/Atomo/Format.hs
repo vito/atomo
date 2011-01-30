0029a2813762a2eaae11ade64aeb5744f9489cb5
Alex Suraci <i.am@toogeneric.com>
2011-01-30 18:58:43 +0000

    give a better error in `i2d`

--- src/Atomo/Format.hs
+++ src/Atomo/Format.hs
@@ -13,7 +13,9 @@ import Atomo.Format.Types
 
 
 i2d :: Int -> Char
-i2d = (!!) (['0'..'9'] ++ ['a'..'z'])
+i2d x
+    | x `elem` [0..36] = (['0'..'9'] ++ ['a'..'z']) !! x
+    | otherwise = error "radix must be between 0 and 36"
 
 format :: Formatter ()
 format = do
